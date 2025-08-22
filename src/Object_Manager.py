8608ff5c5677fc0e62eabe9c97b09b185c29e548
OnlyVSUser <onlyvisualstudiouser@outlook.com>
2025-08-22 06:58:43 -0500

    re-structure

---
+++ src/Object_Manager.py
@@ -0,0 +1,17 @@
+class L_Object:
+    lName = ""
+    lx = 0
+    ly = 0
+    lz = 0
+    RC = False
+    lTexture = ""
+    lShader = ""
+    lShape = ""
+    
+
+to_render = {
+
+}
+
+def create_object(lName, lx, ly, lz, RC, lTexture, lShader, lShape):
+    print("Creating Object")
